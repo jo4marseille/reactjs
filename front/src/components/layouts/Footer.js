45a733b66fd885b5ebb945c3bb951de420d9e17e
4illian <killian.occelli@epitech.eu>
2022-11-14 16:39:34 +0100

    footer fix

--- front/src/components/layouts/Footer.js
+++ front/src/components/layouts/Footer.js
@@ -11,29 +11,64 @@ import PictureCode4Marseille from "../../assets/images/code4marseille.png"
 
 const Footer = () => {
     return (
+        //
+        // <footer className="footer">
+        //     <div className="row">
+        //         <a href="https://www.departement13.fr/" target="_blank" className="column">
+        //             <img src={PictureBoucheDuRhone} alt="d13"/>
+        //         </a>
+        //         <a href="https://membres.voyage-prive.com/voyageprive/fr/?cid=31746&affid=Marque-exact-42638028443[…]NdCvePsNXX2k_o5m2k7P8h7CDTUxLBP9q1SEIj_No1QaAiqSEALw_wcB"
+        //            target="_blank" className="column">
+        //             <img src={PictureVoyagePrive} alt="voyage privé"/>
+        //         </a>
+        //         <a href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank" className="column">
+        //             <img src={PictureAWS} alt="aws"/>
+        //         </a>
+        //     </div>
+        //     <div className="row">
+        //         <a href="https://www.myprovence.fr/" target="_blank" className="column">
+        //             <img className="smaller" src={PictureProvenceTourisme} alt="my provence"/>
+        //         </a>
+        //         <a href="https://inco-group.co/" target="_blank" className="column">
+        //             <img className="smaller" src={PictureInco} alt="inco"/>
+        //         </a>
+        //         <a href="https://code4marseille.fr/" target="_blank" className="column">
+        //             <img className="smaller" src={PictureCode4Marseille} alt="code4marseille"/>
+        //         </a>
+        //     </div>
+        // </footer>
 
-        <footer className="footer">
-            <div className="row">
-                <a href="https://www.departement13.fr/" target="_blank" className="column">
+        <footer class="footer">
+            <div class="row">
+                <a href="https://www.departement13.fr/" target="_blank"
+                   class="column">
                     <img src={PictureBoucheDuRhone} alt="d13"/>
                 </a>
-                <a href="https://membres.voyage-prive.com/voyageprive/fr/?cid=31746&affid=Marque-exact-42638028443[…]NdCvePsNXX2k_o5m2k7P8h7CDTUxLBP9q1SEIj_No1QaAiqSEALw_wcB"
-                   target="_blank" className="column">
-                    <img src={PictureVoyagePrive} alt="voyage privé"/>
+                <a
+                    href="https://membres.voyage-prive.com/voyageprive/fr/?cid=31746&amp;affid=Marque-exact-42638028443[…]NdCvePsNXX2k_o5m2k7P8h7CDTUxLBP9q1SEIj_No1QaAiqSEALw_wcB"
+                    target="_blank" class="column">
+                    <img src={PictureVoyagePrive} alt="d13" class=""/>
                 </a>
-                <a href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank" className="column">
-                    <img src={PictureAWS} alt="aws"/>
+                <a
+                    href="https://inco-group.co/" class="column" target="_blank">
+                    <img class="smaller" src={PictureInco} alt="d13"/>
                 </a>
             </div>
-            <div className="row">
-                <a href="https://www.myprovence.fr/" target="_blank" className="column">
-                    <img className="smaller" src={PictureProvenceTourisme} alt="my provence"/>
+            <div class="row">
+                <a href="https://www.myprovence.fr/" target="_blank"
+                   class="column">
+                    <img src={PictureProvenceTourisme}
+                         alt="d13"
+
+                    />
                 </a>
-                <a href="https://inco-group.co/" target="_blank" className="column">
-                    <img className="smaller" src={PictureInco} alt="inco"/>
+                <a
+                    href="https://code4marseille.fr/" target="_blank" class="column">
+                    <img class="" src={PictureCode4Marseille} alt="d13"/>
                 </a>
-                <a href="https://code4marseille.fr/" target="_blank" className="column">
-                    <img className="smaller" src={PictureCode4Marseille} alt="code4marseille"/>
+                <a
+                    href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank" class="column">
+                    <img src={PictureAWS} alt="d13"/>
                 </a>
             </div>
         </footer>

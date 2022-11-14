6112cf72caca1f096d972a8f4ca19db79f4c4966
Thomas Fouquert <tfouquert@gmail.com>
2022-11-14 18:19:31 +0100

    sponsors index

--- front/src/components/Navigation.jsx
+++ front/src/components/Navigation.jsx
@@ -5,11 +5,10 @@ const Navigation = () =>{
     const navigate = useNavigate();
 
     return(<div>
-        <button onClick={() => navigate("/")} class="btn btn-primary">Home</button>
+        <button onClick={() => navigate("/")} className="btn btn-primary">Home</button>
         <button onClick={() => navigate("/athlete")}>Athlete</button>
         <button onClick={() => navigate("/sponsor")}>Sponsor</button>
     </div>)
 }
 
 export default Navigation;
-

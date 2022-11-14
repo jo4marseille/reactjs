3eacd0e9fbef78aafcc461e40606820768e06642
Jessica4488 <jessica.jouvencel@epitech.eu>
2022-11-14 18:55:23 +0100

    data

---
+++ front/public/data.js
@@ -0,0 +1,70 @@
+{
+    "transports"; [
+      {
+            "name": "Train",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+      },
+      {
+            "name": "Bus",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+      },
+      {
+            "name": "Métro",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+      },
+      {
+        "name": "Métro",
+        "image": "22",
+        "description": "Information Technology",
+        "lien": "123"
+      }
+    ]
+
+    "restaurants"; [
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        },
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        },
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        }
+      ]
+      
+      "hotels"; [
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        },
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        },
+        {
+            "name": "Student 1",
+            "image": "22",
+            "description": "Information Technology",
+            "lien": "123"
+        }
+      ]
+  }

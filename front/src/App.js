4843383a925d7528d4e9de8763094c591dca454d
boussaidm <meriem.boussaid@epitech.eu>
2022-11-14 19:18:27 +0100

    page accueil + footer

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from "react";
+import Footer from "./components/Footer"
+import {createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <></>
   );
 }
 
 export default App;
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element:  <Home/>
+//   },
+
+// ]);
+
+// export default router;
---
+++ front/src/components/Footer.js
@@ -0,0 +1,31 @@
+function Footer() {
+
+    return(
+        <footer className="footer">
+            <div className="row">
+                <a href="https://www.departement13.fr/" target="_blank" className="column">
+                <img src="img/footer/Bouches-du-Rhône_13_logo.png" alt="d13" />
+                </a>
+                <a href="https://membres.voyage-prive.com/voyageprive/fr/?cid=31746&affid=Marque-exact-42638028443[…]NdCvePsNXX2k_o5m2k7P8h7CDTUxLBP9q1SEIj_No1QaAiqSEALw_wcB" target="_blank" className="column">
+                <img src="img/footer/Voyage_Privé.svg.png" alt="voyage privé" />
+                </a>
+                <a href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank" className="column">
+                <img src="img/footer/AWS.png" alt="aws" />
+                </a>
+            </div>
+            <div className="row">
+                <a href="https://www.myprovence.fr/" target="_blank" className="column">
+                <img className="smaller" src="img/footer/provencetourisme.png" alt="my provence"  />
+                </a>
+                <a href="https://inco-group.co/" target="_blank" className="column">
+                <img className="smaller" src="img/footer/inco.png" alt="inco"  />
+                </a>
+                <a href="https://code4marseille.fr/" target="_blank" className="column">
+                <img className="smaller" src="img/footer/code4marseille.png" alt="code4marseille"  />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

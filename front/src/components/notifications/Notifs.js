1fbdec1c4bfb715d181b301f5f29d8c3ef26bd7e
benepitech <benjamin.nicolas@epitech.eu>
2022-11-16 00:32:28 +0100

    jsons

--- front/src/components/notifications/Notifs.js
+++ front/src/components/notifications/Notifs.js
@@ -10,28 +10,28 @@ function Notifs() {
     useEffect(() => {
         const timer = setTimeout(() => {
             return NotificationManager.info('Football : Victoire du Brésil contre l\'Allemagne 7-2');
-        }, 5000);
-        return () => clearTimeout(timer);
-    }, []);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            return NotificationManager.info('Tennis : Tsonga vs Ferrero début du match dans 5 minutes sur France 4');
-        }, 10000);
-        return () => clearTimeout(timer);
-    }, []);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            return NotificationManager.info('Sprint 100m : Nouveau record du monde d\'Usain Bolt 9,51 secondes');
         }, 20000);
         return () => clearTimeout(timer);
     }, []);
 
     useEffect(() => {
         const timer = setTimeout(() => {
+            return NotificationManager.info('Tennis : Tsonga vs Ferrero début du match dans 5 minutes sur France 4');
+        }, 25000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return NotificationManager.info('Sprint 100m : Nouveau record du monde d\'Usain Bolt 9,51 secondes');
+        }, 45000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
             return NotificationManager.info("Natation Médaille d'or pour l'équipe de France");
-        }, 40000);
+        }, 65000);
         return () => clearTimeout(timer);
     }, []);
 

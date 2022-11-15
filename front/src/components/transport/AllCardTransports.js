2210886aa8980cf3232e49ffa5869a4494e16340
Jessica4488 <jessica.jouvencel@epitech.eu>
2022-11-15 15:09:24 +0100

    màjallcards

--- front/src/components/transport/AllCardTransports.js
+++ front/src/components/transport/AllCardTransports.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 function AllCardTransports() {
 
     return(<div className="componentBg">
-        {data.tra.map((e) => {
+        {data.transports.map((e) => {
 
             return(
                 <Card name={e.name} description={e.description}/>

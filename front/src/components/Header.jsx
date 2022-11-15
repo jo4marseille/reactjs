8e370b24fc2c92a2516c54a54ac95d10db42354d
JordanDeledda <jordan.deledda@epitech.eu>
2022-11-15 09:56:55 +0100

    Navbar

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid d-flex flex-row">
-                    <a className="navbar-brand" href="#"><img src="favicon.ico" alt="" /></a>
+                    <a className="navbar-brand flex-grow-1" href="#"><img src="favicon.ico" alt="" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul className='navbar-nav mr-auto mt-2 mt-lg-0 ms-auto'>
                             <li className='nav-item active'>
                                 <a className="nav-link px-2" href='/'>Home</a>
                             </li>

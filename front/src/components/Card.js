79f0dddef4dd90f36cc1ea70d9750be8ec88d2a0
VetsoAndria <vetso@ymail.com>
2022-11-15 16:06:41 +0100

    modif

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 function Cards(props) {
 
@@ -7,18 +8,18 @@ function Cards(props) {
 
   return (
 
-    <>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>{props.name}</Card.Title>
-        <Card.Text>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-
+    <> 
+   
+      <Card style={{ width: '18rem' }} className="card">
+        <Card.Img class="card-img-top" src={props.image} />
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>
+            {props.description}
+          </Card.Text>
+          <Link to={props.lien}> <Button variant="primary">En savoir plus</Button></Link>
+        </Card.Body>
+      </Card>
     </>
   );
 }

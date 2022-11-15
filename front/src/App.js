594d6ecba09a4edccca53a1539c488dce0d4f1b9
Florent-F-Dev <68734060+Florent-F-Dev@users.noreply.github.com>
2022-11-15 10:16:08 +0100

    Florent - footer

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
 import Footer from "./components/footer/Footer";
+import Accueil from "./pages/Accueil";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path={"/"} element={<Home/>} />
-            <Route path={"/*"} element={<Home/>} />
+            <Route path={"/"} element={<Accueil/>} />
+            <Route path={"/*"} element={<Accueil/>} />
         </Routes>
-        <Footer/>
     </BrowserRouter>
   );
 }
--- front/src/pages/Accueil.jsx
+++ front/src/pages/Accueil.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from "../components/footer/Footer";
 
 const Accueil = () => {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     );
 };

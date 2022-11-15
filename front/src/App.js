bbae9f6035a783e4b0bfaa30b2e1773e64d9252f
cpeloux@voyageprive.com <cpeloux@voyageprive.com>
2022-11-15 10:45:02 +0100

    login

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+//import Signin from './Signin/Signin';
+import Signup from './Signup/Signup';
 import './App.css';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        {/* <Signin /> */}
+        <Signup />
+      </div>
+    </AuthProvider>
   );
 }
 

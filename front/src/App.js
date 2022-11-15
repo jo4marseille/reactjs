a1c72c8c41257bf3b269b9265a19162ca0fdc2b2
benepitech <benjamin.nicolas@epitech.eu>
2022-11-15 18:58:37 +0100

    jsons

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Rooter from "./router/Rooter";
+import Notifs from "./components/notifications/Notifs";
+import * as React from "react";
 
 function App() {
     return (
         <div>
             <Rooter/>
+            <Notifs/>
         </div>
     );
 }
---
+++ front/src/components/notifications/Notifs.js
@@ -0,0 +1,46 @@
+import React, {useEffect} from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+
+import 'react-notifications/lib/notifications.css'
+import './Notifs.css'
+
+
+function Notifs() {
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return NotificationManager.info('Football : Victoire du Brésil contre l\'Allemagne 7-2');
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return NotificationManager.info('Tennis : Tsonga vs Ferrero début du match dans 5 minutes sur France 4');
+        }, 10000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return NotificationManager.info('Sprint 100m : Nouveau record du monde d\'Usain Bolt 9,51 secondes');
+        }, 20000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return NotificationManager.info("Natation Médaille d'or pour l'équipe de France");
+        }, 40000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div>
+            <NotificationContainer/>
+        </div>
+    );
+
+}
+
+export default Notifs;

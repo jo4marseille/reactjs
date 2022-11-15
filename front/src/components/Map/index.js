572edf079f2c3ffab0ffa38ba7fa90f46f74b32e
MartinGab <martingabillet@gmail.com>
2022-11-15 14:11:15 +0100

    fausses données hotels + events

--- front/src/components/Map/index.js
+++ front/src/components/Map/index.js
@@ -9,6 +9,9 @@ import './map.css';
 
 import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
 
+import eventsLocation from '../../data/events';
+import hotels from '../../data/hotels';
+
 const containerStyle = {
     width: '100%',
     height: '100%'
@@ -34,6 +37,21 @@ const Map = () => {
 
     return (
         <div className="map-container">
+            <div className="search">
+                <select name="searchFrom" id="searchFrom" className="search__from search-input">
+                    <option value="">Choisissez l'event</option>
+                    {eventsLocation.map((item) => (
+                        <option value={item.id}>{item.name}</option>
+                    ))}
+                </select>
+                <select name="searchTo" id="searchTo" className="search__to search-input">
+                    <option value="">Choisissez vôtre hôtel</option>
+                    {hotels.map((item) => (
+                        <option value={item.id}>{item.name}</option>
+                    ))}
+                </select>
+                <button className="search__submit">Rechercher</button>
+            </div>
             <GoogleMap
                 center={mapCenter}
                 mapContainerStyle={containerStyle}
---
+++ front/src/data/events.js
@@ -0,0 +1,20 @@
+const eventsLocation = [
+    {
+        "id": 1,
+        "name": "Orange Vélodrome",
+        "coordinates": {
+            "lat": 43.2699988435189,
+            "lng": 5.395865840039364,
+        },
+    },
+    {
+        "id": 2,
+        "name": "Marina",
+        "coordinates": {
+            "lat": 43.2671273806127,
+            "lng": 5.372170719199464,
+        },
+    }
+];
+
+export default eventsLocation;

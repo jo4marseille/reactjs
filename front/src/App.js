154b53b0c9f3f420655821bcab1e84c89ea39224
Jessica4488 <jessica.jouvencel@epitech.eu>
2022-11-15 13:10:53 +0100

    modifdataetcard

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,16 @@ import Card from './components/Card';
 import './App.css';
 import NavBar  from "./components/Navbar";
 
+
 function App() {
   return (
     <div className="App">
-<NavBar/> 
-<br/>
-<Card/>
-</div>
+      <NavBar/> 
+      <Card/>
+    </div>
 
 );
 }
+
 
 export default App;

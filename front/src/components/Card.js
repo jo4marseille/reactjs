895c3803638a39d3bd82c215c9104af28c07b833
VetsoAndria <vetso@ymail.com>
2022-11-15 17:00:57 +0100

    add modif data

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -10,8 +10,8 @@ function Cards(props) {
 
     <> 
    
-      <Card style={{ width: '18rem' }} className="card">
-        <Card.Img class="card-img-top" src={props.image} />
+      <Card style={{ width: '18rem',margin:'4px' }} className="d-inline-flex">
+        <Card.Img className="card-img-top" src={props.image} />
         <Card.Body>
           <Card.Title>{props.name}</Card.Title>
           <Card.Text>
@@ -19,7 +19,9 @@ function Cards(props) {
           </Card.Text>
           <Link to={props.lien}> <Button variant="primary">En savoir plus</Button></Link>
         </Card.Body>
+        
       </Card>
+      
     </>
   );
 }
--- front/src/components/lodging/AllCardLodgings.js
+++ front/src/components/lodging/AllCardLodgings.js
@@ -6,7 +6,6 @@ function AllCardLodgings() {
     return(<div className="componentBg">
     {data.hotels.map((e) => {
 
-<<<<<<< HEAD
         return(
             
             <Cards name={e.name} description={e.description} lien= {e.lien} image={e.image}/>
@@ -14,79 +13,6 @@ function AllCardLodgings() {
     }
     )}
         </div>);
-=======
-
-
-        <div class="box">
-        <img src="\img\logos\logoHotel.png" alt='logo hotel' id="logo"/>
-            <div class="container">
-                 <div class="row">
-
-                         
-                        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                       
-                            <div className="box-part text-center">
-                                
-                                <i className="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
-                                
-                                <div className="title">
-                                    <h4>Hébergement </h4>
-                                </div>
-                                
-                                <div className="text">
-                                    <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                                </div>
-                                
-                                <a href="#">Learn More</a>
-                                
-                             </div>
-                        </div>	 
-                        
-                         <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                       
-                            <div className="box-part text-center">
-                                
-                                <i className="fa fa-google-plus fa-3x" aria-hidden="true"></i>
-                            
-                                <div className="title">
-                                    <h4>Hébergement2</h4>
-                                </div>
-                                
-                                <div className="text">
-                                    <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                                </div>
-                                
-                                <a href="#">Learn More</a>
-                                
-                             </div>
-                        </div>	 
-                        
-                         <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                       
-                            <div className="box-part text-center">
-                                
-                                <i className="fa fa-github fa-3x" aria-hidden="true"></i>
-                                
-                                <div className="title">
-                                    <h4>Hébergement3</h4>
-                                </div>
-                                
-                                <div className="text">
-                                    <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                                </div>
-                                
-                                <a href="#">Learn More</a>
-                                
-                             </div>
-                        </div>
-                
-                </div>		
-            </div>
-        </div>
-        
-        
-            </div>);
->>>>>>> c1111ab51f05a49eb76cb769e26f6799b75a5a24
     
 }
 export default AllCardLodgings

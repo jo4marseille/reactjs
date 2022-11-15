fdde6e8d93738f2be7c8bfa4ffcdc5066206a562
Guillaume Teillais <guillaume.teillais@gmail.com>
2022-11-15 13:21:06 +0100

    header ok + refonte footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -10,28 +10,25 @@ import Voyage_prive from "../assets/images/voyage_prive.png";
 function Footer() {
   return (
     <footer class="footer">
-      <div class="row">
-        <a href="https://www.departement13.fr/" target="_blank" class="column">
+      
+        <a href="https://www.departement13.fr/" target="_blank">
           <img src={Departement13} alt="d13" />
         </a>
-        <a href="https://membres.voyage-prive.com/voyageprive/fr/?cid=31746&affid=Marque-exact-42638028443[…]NdCvePsNXX2k_o5m2k7P8h7CDTUxLBP9q1SEIj_No1QaAiqSEALw_wcB" target="_blank" class="column">
+        <a href="https://www.voyage-prive.com/" target="_blank">
         <img src={Voyage_prive} alt="voyage privé" />
         </a>
-        <a href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank" class="column">
+        <a href="https://aws.amazon.com/fr/?nc2=h_lg" target="_blank">
         <img src={Aws} alt="aws" />
         </a>
-      </div>
-      <div class="row">
-        <a href="https://www.myprovence.fr/" target="_blank" class="column">
+        <a href="https://www.myprovence.fr/" target="_blank">
         <img class="smaller" src={Provencetourisme} alt="my provence"  />
         </a>
-        <a href="https://inco-group.co/" target="_blank" class="column">
+        <a href="https://inco-group.co/" target="_blank">
         <img class="smaller" src={Inco} alt="inco" />
         </a>
-        <a href="https://code4marseille.fr/" target="_blank" className="column">
+        <a href="https://code4marseille.fr/" target="_blank">
         <img src={Code4marseille} alt="code4marseille"  />
         </a>
-      </div>
     </footer>
   )
 }

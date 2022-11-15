083969d8c2737216ff10dc1267ef8989890c11f4
4illian <killian.occelli@epitech.eu>
2022-11-15 15:27:21 +0100

    fix

--- front/src/views/Agenda.js
+++ front/src/views/Agenda.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import Multiselect from '../components/filters/Multiselect'
+import * as React from 'react';
+import {useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import FilterBar from "../components/agenda/FilterBar";
+import {useState} from "react";
+import VerticalContainer from "../components/agenda/VerticalContainer";
 
-
+import "./Agenda.css"
 function Agenda() {
+
+    let [filterList, setFilterList] = useState([])
+
+
     return (
-        <div>
-            <Multiselect/>
-Agenda view
+
+        <div className="div_ctn_agenda">
+            <FilterBar filterList={filterList} setFilterList={setFilterList}/>
+
+
+
+
+
+            <VerticalContainer/>
+
+
         </div>
-    );
-};
+    )
+}
 
 export default Agenda;

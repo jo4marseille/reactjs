2c515516fb1afba5e43be591c15739484aaf3ef1
MartinGab <martingabillet@gmail.com>
2022-11-15 13:08:01 +0100

    first display of map

--- front/src/components/Map/index.js
+++ front/src/components/Map/index.js
@@ -7,10 +7,41 @@
 *****************/
 import './map.css';
 
-const Map = () => (
-    <div className="map-container">
-        Map Component
-    </div>
-);
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+
+const containerStyle = {
+    width: '100%',
+    height: '100%'
+};
+
+const mapCenter = {
+    lat: 43.268178,
+    lng: 5.384399
+};
+
+const Map = () => {
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    });
+
+    if (!isLoaded) {
+        return (
+            <div className="map-container">
+                Map is loading...
+            </div>
+        )
+    };
+
+    return (
+        <div className="map-container">
+            <GoogleMap
+                center={mapCenter}
+                mapContainerStyle={containerStyle}
+                zoom={13}
+            >
+            </GoogleMap>
+        </div>
+    );
+};
 
 export default Map;

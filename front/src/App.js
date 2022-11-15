450120b93323a37d5b34ce85569f1a89e0a04640
boussaidm <meriem.boussaid@epitech.eu>
2022-11-15 10:11:18 +0100

    route home + composant footer ajoutés

--- front/src/App.js
+++ front/src/App.js
@@ -13,6 +13,8 @@ import AllCardTransports from './components/transport/AllCardTransports';
 import AllCardLodgings from './components/lodging/AllCardLodgings';
 import AllCardRestaurations from './components/restauration/AllCardRestaurations';
 import AllCardSports from './components/sport/AllCardSports';
+import Footer from './components/Footer';
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -20,11 +22,13 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
+          <Route exact path="/home"  element={<Home />}/>      
           <Route exact path="/transport"  element={<AllCardTransports />}/>      
           <Route path="/hebergements" element={<AllCardLodgings />} />   
           <Route path="/restauration"element={<AllCardRestaurations />} />
           <Route path="/sport" element={<AllCardSports />} />
         </Routes>
+        <Footer/>
       </Router>
 
     </div>

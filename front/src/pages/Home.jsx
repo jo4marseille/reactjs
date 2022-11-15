b32895715e55b5aa0c84b4e0a7c2166934ac8822
Florent-F-Dev <68734060+Florent-F-Dev@users.noreply.github.com>
2022-11-15 14:39:10 +0100

    Florent - home premier mot

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -5,18 +5,18 @@ import Footer from '../components/footer/Footer';
 const Home = () => {
     return (
         <div className={"home"}>
-            <div className={"home-sub-1"}></div>
-            <div className={"home-sub-2"}></div>
-            <div className={"home-sub-3"}></div>
-            <div className={"home-sub-4"}></div>
-            <div className={"home-sub-5"}></div>
-            <div className={"home-sub-6"}></div>
-            <div className={"home-sub-7"}></div>
-            <div className={"home-sub-8"}></div>
-            <div className={"home-sub-9"}></div>
-            <div className={"home-sub-10"}></div>
-            <div className={"home-sub-11"}></div>
-            <div className={"home-sub-12"}></div>
+                <div className={"home-sub-1"}><span>AGEN</span></div>
+                <div className={"home-sub-2"}><span>DA</span></div>
+                <div className={"home-sub-3"}></div>
+                <div className={"home-sub-4"}></div>
+                <div className={"home-sub-5"}></div>
+                <div className={"home-sub-6"}></div>
+                <div className={"home-sub-7"}></div>
+                <div className={"home-sub-8"}></div>
+                <div className={"home-sub-9"}></div>
+                <div className={"home-sub-10"}></div>
+                <div className={"home-sub-11"}></div>
+                <div className={"home-sub-12"}></div>
         </div>
     );
 };

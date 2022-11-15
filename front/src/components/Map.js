19fb9808ffda9a59f939f92ada8a4a8295cf47a6
Mérina Achouri <merina.achouri@outlook.fr>
2022-11-15 15:34:29 +0100

    page home

--- front/src/components/Map.js
+++ front/src/components/Map.js
@@ -29,6 +29,7 @@ export default function Map() {
         <img
           src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg"
           alt=""
+          className="map"
         />
       </div>
     </>
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,11 +1,53 @@
 import Card from "react-bootstrap/Card";
+import fond from "./fond.jpg";
+import voile from "./voile.png";
+import foot from "./foot.jpg";
 import "../App.css";
 
 function Home() {
   return (
     <>
-      <p>Test</p>
-      {/* <img src="logo.png" alt="" /> */}
+      <div className="all">
+        <div className="square">
+          <img src={fond} alt="" className="fond" />
+          <div className="presentation">
+            <h1 className="pres-title">Marseille,</h1>
+            <h1 className="pres-title">terroir des JO</h1>
+
+            <p className="pres-description">
+              Découvrez l'authentique ville de Marseille derrière les Jeux
+              Olympiques ! Quel monument visiter à côté de La Marina ? Où manger
+              une bouillabaisse près du Vélodrome ?
+              <span>
+                Retrouvez toutes les informations indispensables à votre visite
+                !
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="choose-events">
+          <h2 className="choose-title">Quel événement allez-vous voir ?</h2>
+          <div className="cards">
+            <div className="card-voile">
+              <img src={voile} alt="" className="voile" />
+              <p>
+                ÉPREUVES <br />
+                OLYMPIQUES
+                <br />
+                DE VOILE
+              </p>
+            </div>
+            <div className="card-foot">
+              <img src={foot} alt="" className="foot" />
+              <p>
+                ÉPREUVES <br />
+                OLYMPIQUES
+                <br /> DE FOOTBALL
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
